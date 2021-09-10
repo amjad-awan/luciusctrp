ba46311f654c4a08a880d597dc7130cc4e533029
amjad-awan <73718759+amjad-awan@users.noreply.github.com>
2021-09-10 09:21:34 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".menu-toggle-btn").click(function(){
+        
+        $(this).toggleClass("fa-times");
+        $('.navigation-menu').toggleClass("active");
+    })
+})
+
+
+
